d1f13f30a34d30a31731b1257e9603016f6f8f83
Rudy <rudy@yetihq.com>
2013-04-12 16:03:07 -0700

    bare id field for generic foreign key field

--- manticore_tastypie_core/fields.py
+++ manticore_tastypie_core/fields.py
@@ -1,3 +1,4 @@
+from tastypie.contrib.contenttypes.fields import GenericForeignKeyField
 from tastypie.exceptions import ApiFieldError
 from tastypie.fields import ToOneField
 
@@ -12,7 +13,6 @@ class ToBareForeignKeyField(ToOneField):
         self.fk_resource = self.to_class()
 
         #TODO: Dynamically pull api path
-        # value = "%s/%s/" % (self.fk_resource.Meta.resource_name, value)
         value = "/api/v1/%s/%s/" % (self.fk_resource.Meta.resource_name, value)
 
         return super(ToBareForeignKeyField, self).build_related_resource(value, request, related_obj, related_name)
@@ -32,3 +32,16 @@ class ToBareForeignKeyField(ToOneField):
             return value
         else:
             raise ApiFieldError("The model '%r' doesn't have an id for attribute '%s'" % (bundle.obj, self.attribute))
+
+
+# Expects there to be only one model and resource pair in the generic foreign key
+class BareGenericForeignKeyField(GenericForeignKeyField):
+
+    def build_related_resource(self, value, request=None, related_obj=None, related_name=None):
+        if not isinstance(value, int):
+            raise ApiFieldError("The '%s' field was given non-integer data: %s." % (self.instance_name, value))
+
+        #TODO: Dynamically pull api path)
+        value = "/api/v1/%s/%s/" % (self.to.itervalues().next().Meta.resource_name, value)
+
+        return super(BareGenericForeignKeyField, self).build_related_resource(value, request, related_obj, related_name)
