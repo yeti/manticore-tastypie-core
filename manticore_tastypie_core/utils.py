dd0815323a9f25067257a66d2735c200aa385803
Rudy <rudy@yetihq.com>
2013-04-15 12:01:23 -0700

    location range util functions

--- manticore_tastypie_core/utils.py
+++ manticore_tastypie_core/utils.py
@@ -1,3 +1,4 @@
+import math
 from manticore_tastypie_core.manticore_tastypie_core.resources import GooglePlaceResource, LocationResource
 
 __author__ = 'rudolphmutter'
@@ -8,3 +9,20 @@ def register_api(api):
     api.register(GooglePlaceResource())
     api.register(LocationResource())
     return api
+
+
+# 111.12 km = 1 degree of latitude
+# distance in km
+# ref http://www.scribd.com/doc/2569355/Geo-Distance-Search-with-MySQL, slide 12
+def get_longitude_range(longitude, latitude, dist):
+    lon1 = longitude - dist / abs(math.cos(math.radians(latitude)) * 111.12)
+    lon2 = longitude + dist / abs(math.cos(math.radians(latitude)) * 111.12)
+
+    return lon1, lon2
+
+
+def get_latitude_range(latitude, dist):
+    lat1 = latitude - (dist / 111.12)
+    lat2 = latitude + (dist / 111.12)
+
+    return lat1, lat2
