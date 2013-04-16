b6eef35c7ac8bcdb34f2a9e5ef408fd300cb777f
Rudy <rudy@yetihq.com>
2013-04-15 18:39:21 -0700

    add more fields for location

--- manticore_tastypie_core/models.py
+++ manticore_tastypie_core/models.py
@@ -8,10 +8,14 @@ __author__ = 'rudolphmutter'
 class Location(CoreModel):
     # Google Place unique ID
     id = models.CharField(max_length=250, primary_key=True)
-    name = models.CharField(max_length=125)
-    address = models.CharField(max_length=125)
+    name = models.CharField(max_length=125, null=True, blank=True)
+    address = models.CharField(max_length=125, null=True, blank=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    zipcode = models.CharField(max_length=10, null=True, blank=True)
+    country = models.CharField(max_length=40)
 
     def __unicode__(self):
         return u"%s" % self.name
