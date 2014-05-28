0e5f029e97f1845a622eb0b0c11a23f309fa4e25
Rudy Mutter <rudy@yetihq.com>
2014-05-28 12:22:01 -0700

    removing location dependancy to separate library

--- manticore_tastypie_core/resources.py
+++ manticore_tastypie_core/resources.py
@@ -1,9 +1,7 @@
-from _ssl import SSLError
 from django.conf import settings
 from django.conf.urls import url
 from django.db import IntegrityError
 from django.db.models import FieldDoesNotExist
-from googleplaces import GooglePlaces, ranking
 from tastypie import http, fields
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
@@ -11,8 +9,6 @@ from tastypie.bundle import Bundle
 from tastypie.exceptions import BadRequest
 from tastypie.resources import ModelResource, Resource
 from manticore_django.manticore_django.utils import retry_cloudfiles
-from manticore_tastypie_core.manticore_tastypie_core.models import Location
-from manticore_tastypie_user.manticore_tastypie_user.authentication import ExpireApiKeyAuthentication
 
 
 class BaseModelResource(ModelResource):
@@ -165,92 +161,6 @@ class PictureVideoUploadResource(ManticoreModelResource):
 
         # Retry cloudfiles in case we get a 404 because the videos haven't finished uploading yet
         return retry_cloudfiles(return_response, self, request, bundle)
-
-
-class GooglePlace(object):
-    def __init__(self, id=None, name=None, address=None):
-        self.id = id
-        self.name = name
-        self.address = address
-
-
-class GooglePlaceResource(ManticoreResource):
-    id = fields.CharField(attribute='id')
-    name = fields.CharField(attribute='name')
-    address = fields.CharField(attribute='vicinity', null=True)
-    latitude = fields.FloatField()
-    longitude = fields.FloatField()
-
-    class Meta:
-        resource_name = 'place'
-        allowed_methods = ['get']
-        object_class = GooglePlace
-        authorization = Authorization()
-        authentication = ExpireApiKeyAuthentication()
-        always_return_data = True
-        object_name = "place"
-
-    def dehydrate_latitude(self, bundle):
-        return bundle.obj.geo_location['lat']
-
-    def dehydrate_longitude(self, bundle):
-        return bundle.obj.geo_location['lng']
-
-    def _client(self):
-        return GooglePlaces(settings.GOOGLE_API_KEY)
-
-    def detail_uri_kwargs(self, bundle_or_obj):
-        kwargs = {}
-
-        if isinstance(bundle_or_obj, Bundle):
-            kwargs['pk'] = bundle_or_obj.obj.id
-        else:
-            kwargs['pk'] = bundle_or_obj['id']
-
-        return kwargs
-
-    def get_object_list(self, bundle, **kwargs):
-        latitude = bundle.request.GET.get('latitude')
-        longitude = bundle.request.GET.get('longitude')
-
-        if latitude and longitude:
-            lat_lng = {"lat": latitude, "lng": longitude}
-        else:
-            raise BadRequest("Need latitude and longitude")
-
-        query_name = bundle.request.GET.get('query', "")
-        if query_name != "":
-            query = self._client().text_search(query=query_name, radius=None)
-        else:
-            query = self._client().nearby_search(sensor=True, lat_lng=lat_lng, radius=8000)
-
-        return query.places
-
-    def obj_get_list(self, bundle, **kwargs):
-        return self.get_object_list(bundle, **kwargs)
-
-    def obj_get(self, request=None, **kwargs):
-        result = self._client().get_place(kwargs['pk'])
-        result.get_details()
-        return result
-
-
-class LocationResource(ManticoreModelResource):
-    class Meta:
-        queryset = Location.objects.all()
-        allowed_methods = ['get', 'post']
-        authorization = Authorization()
-        authentication = ExpireApiKeyAuthentication()
-        resource_name = "location"
-        object_name = "location"
-        filtering = {
-            'id': ['exact'],
-            'name': ['exact'],
-            'neighborhood': ['exact'],
-            'city': ['exact'],
-            'state': ['exact'],
-            'country_code': ['exact', 'iexact'],
-        }
 
 
 class Version(object):
--- manticore_tastypie_core/utils.py
+++ manticore_tastypie_core/utils.py
@@ -1,29 +1,9 @@
-import math
-from manticore_tastypie_core.manticore_tastypie_core.resources import GooglePlaceResource, LocationResource, VersionResource
+from manticore_tastypie_core.manticore_tastypie_core.resources import VersionResource
 
 __author__ = 'rudolphmutter'
 
 
 # Registers this library's resources
 def register_api(api):
-    api.register(GooglePlaceResource())
-    api.register(LocationResource())
     api.register(VersionResource())
     return api
-
-
-# 111.12 km = 1 degree of latitude
-# distance in km
-# ref http://www.scribd.com/doc/2569355/Geo-Distance-Search-with-MySQL, slide 12
-def get_longitude_range(longitude, latitude, dist):
-    lon1 = longitude - dist / abs(math.cos(math.radians(latitude)) * 111.12)
-    lon2 = longitude + dist / abs(math.cos(math.radians(latitude)) * 111.12)
-
-    return lon1, lon2
-
-
-def get_latitude_range(latitude, dist):
-    lat1 = latitude - (dist / 111.12)
-    lat2 = latitude + (dist / 111.12)
-
-    return lat1, lat2
